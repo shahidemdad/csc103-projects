3458fca2cd35914776ab2449df739909fa856ff1
csc103 student <me@citymail.ccny.cuny.edu>
2020-05-26 20:19:06 -0400

    final version

--- p2/primetest.cpp
+++ p2/primetest.cpp
@@ -15,7 +15,33 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int main()
-{
-	return 0;
+int main() {
+
+      int n=0; // input an interger
+      int d;
+      bool ans= true;
+      // number will divide by
+      cout << "enter a number: \n"; // asking for an number
+
+    while (cin >> n) { // inputing number
+
+      for (d=2; d<n ; d++) {
+        if (n%d==0) {
+          //cout << "for loop, in if\n";
+          ans = false;
+          break;
+        }
+        else{
+          //cout << "for loop, in else\n";
+          ans = true;
+        }
+       }
+      if ( ans == true && n>1) {
+          cout << "1\n";
+      }
+      else {
+        cout << "0\n";
+      }
+    }
+return 0;
 }
